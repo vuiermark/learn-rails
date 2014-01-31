a0fb981b52f071ed9a5cd44cec828109d1c0d923
Mark Rybarczyk <stealthmark@gmail.com>
2014-01-31 10:39:15 -0500

    revised application

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,17 +79,17 @@ LearnRails::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # email enabled in production
-  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] } 
-  config.action_mailer.delivery_method = :smtp 
-  config.action_mailer.perform_deliveries = true 
-  config.action_mailer.raise_delivery_errors = false 
+  config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["DOMAIN_NAME"], 
-    authentication: "plain", 
-    enable_starttls_auto: true, 
-    user_name: ENV["GMAIL_USERNAME"], 
+    domain: ENV["DOMAIN_NAME"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
-￼￼￼￼
+  }￼
 end
