c115412e041ced38586078282f13172dbc609c6c
Mark Rybarczyk <stealthmark@gmail.com>
2014-02-01 07:51:59 -0500

    change config env prod to set url in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ LearnRails::Application.configure do
     password: ENV["GMAIL_PASSWORD"]
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => localhost:3000 }
+  config.action_mailer.default_url_options = { :host => https://evening-journey-9828.herokuapp.com }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
